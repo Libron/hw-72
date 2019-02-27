b8dcde9c1e3f01a4b7dfd611bb51811ce2f19f16
Urmat Bekboev <bek.urmat@gmail.com>
2019-02-28 02:15:59 +0600

    Some minor fixes

--- src/components/DishForm/DishForm.js
+++ src/components/DishForm/DishForm.js
@@ -1,80 +1,47 @@
 import React, {Component} from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class DishForm extends Component {
-    componentDidMount() {
-        console.log(this.props.match.params.id);
+    constructor(props) {
+        super(props);
+
+        if (props.dish) {
+            this.state = {...props.dish};
+        } else {
+            this.state = {
+                name: '',
+                price: '',
+                imgURL: '',
+            };
+        }
     }
+
+    valueChanged = event => {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+    };
+
+    submitHandler = event => {
+        event.preventDefault();
+        this.props.submit(this.props.id, {...this.state});
+    };
+
     render() {
         return (
-            <Form>
+            <Form onSubmit={this.submitHandler}>
                 <FormGroup>
-                    <Label for="exampleEmail">Email</Label>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                    <Label for="name">Name</Label>
+                    <Input type="text" name="name" id="name" value={this.state.name} onChange={this.valueChanged} />
                 </FormGroup>
+
                 <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                    <Label for="price">Price</Label>
+                    <Input type="text" name="price" id="price"  value={this.state.price} onChange={this.valueChanged} />
                 </FormGroup>
+
                 <FormGroup>
-                    <Label for="exampleSelect">Select</Label>
-                    <Input type="select" name="select" id="exampleSelect">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleSelectMulti">Select Multiple</Label>
-                    <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleText">Text Area</Label>
-                    <Input type="textarea" name="text" id="exampleText" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleFile">File</Label>
-                    <Input type="file" name="file" id="exampleFile" />
-                    <FormText color="muted">
-                        This is some placeholder block-level help text for the above input.
-                        It's a bit lighter and easily wraps to a new line.
-                    </FormText>
-                </FormGroup>
-                <FormGroup tag="fieldset">
-                    <legend>Radio Buttons</legend>
-                    <FormGroup check>
-                        <Label check>
-                            <Input type="radio" name="radio1" />{' '}
-                            Option one is this and that—be sure to include why it's great
-                        </Label>
-                    </FormGroup>
-                    <FormGroup check>
-                        <Label check>
-                            <Input type="radio" name="radio1" />{' '}
-                            Option two can be something else and selecting it will deselect option one
-                        </Label>
-                    </FormGroup>
-                    <FormGroup check disabled>
-                        <Label check>
-                            <Input type="radio" name="radio1" disabled />{' '}
-                            Option three is disabled
-                        </Label>
-                    </FormGroup>
-                </FormGroup>
-                <FormGroup check>
-                    <Label check>
-                        <Input type="checkbox" />{' '}
-                        Check me out
-                    </Label>
+                    <Label for="imgURL">Image URL</Label>
+                    <Input type="text" name="imgURL" id="imgURL" value={this.state.imgURL} onChange={this.valueChanged} />
                 </FormGroup>
                 <Button>Submit</Button>
             </Form>
